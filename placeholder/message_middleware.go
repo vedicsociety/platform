62084606120869675801adf844780c6514795e70
albatros <tsiparinda@hotmail.com>
2022-08-06 14:09:14 +0300

    Part3

--- placeholder/message_middleware.go
+++ placeholder/message_middleware.go
@@ -1,35 +1,36 @@
 package placeholder
 
 import (
-    //"io"
-    //"errors"
-    "platform/pipeline"
-    "platform/config"
-    //"platform/services"
-    "platform/templates"
+	//"io"
+	//"errors"
+	"platform/config"
+	"platform/pipeline"
+
+	//"platform/services"
+	"platform/templates"
 )
 
 type SimpleMessageComponent struct {
-    Message string
-    config.Configuration
+	Message string
+	config.Configuration
 }
 
 func (lc *SimpleMessageComponent) ImplementsProcessRequestWithServices() {}
 
 func (c *SimpleMessageComponent) Init() {
-    c.Message = c.Configuration.GetStringDefault("main:message",  
-        "Default Message") 
+	c.Message = c.Configuration.GetStringDefault("main:message",
+		"Default Message")
 }
 
 func (c *SimpleMessageComponent) ProcessRequestWithServices(
-    ctx *pipeline.ComponentContext, 
-    next func(*pipeline.ComponentContext), 
-    executor templates.TemplateExecutor)  {
-    err := executor.ExecTemplate(ctx.ResponseWriter, 
-        "simple_message.html", c.Message)
-    if (err != nil) {
-        ctx.Error(err)
-    } else {
-        next(ctx)
-    }
+	ctx *pipeline.ComponentContext,
+	next func(*pipeline.ComponentContext),
+	executor templates.TemplateExecutor) {
+	err := executor.ExecTemplate(ctx.ResponseWriter,
+		"simple_message.html", c.Message)
+	if err != nil {
+		ctx.Error(err)
+	} else {
+		next(ctx)
+	}
 }
